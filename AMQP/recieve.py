98cde3a521dd407922f77c1135d1aafbf6345872
Shyam <shyam.sudhakaran97@gmail.com>
2018-04-16 00:22:31 -0700

    Added visualizations and topics/routing_keys

--- AMQP/recieve.py
+++ AMQP/recieve.py
@@ -1,18 +1,63 @@
 #!/usr/bin/env python
 import pika
+import sys 
+import pandas as pd
+import matplotlib.pyplot as plt
+import networkx as nx
+import seaborn as sns
 
 connection = pika.BlockingConnection(pika.ConnectionParameters('127.0.0.1'))
 channel = connection.channel()
 
 
-channel.queue_declare(queue='hello')
+channel.exchange_declare(exchange='topics',
+                         exchange_type='topic')
+
+
+result = channel.queue_declare(exclusive=True)
+queue_name = result.method.queue
+
+message_dict = {}
+binding_keys = sys.argv[1:]
+if not binding_keys:
+    sys.stderr.write("Usage: %s [binding_key]...\n" % sys.argv[0])
+    sys.exit(1)
+
+for binding_key in binding_keys:
+    channel.queue_bind(exchange='topics',
+                       queue=queue_name,
+                       routing_key=binding_key)
+
+print(' [*] Waiting for logs. To exit press CTRL+C')
 
 def callback(ch, method, properties, body):
     print(" [x] Received %r" % body)
+    if method.routing_key in message_dict:
+    	message_dict[method.routing_key].append(body.decode('utf-8'))
+    else:
+    	message_dict[method.routing_key] = [body.decode('utf-8')]
 
 channel.basic_consume(callback,
-                      queue='hello',
+                      queue=queue_name,
                       no_ack=True)
 
-print(' [*] Waiting for messages. To exit press CTRL+C')
-channel.start_consuming()
+try:
+    channel.start_consuming()
+except KeyboardInterrupt:
+    channel.stop_consuming()
+    counts = []
+    for key in list(message_dict.keys()):
+    	counts.append(len(message_dict[key]))
+    plt.figure(figsize=(10,10))
+    plt.title('Number of Messages')
+    freqdf = pd.DataFrame({'routing_key':list(message_dict.keys()),"counts":counts})
+    sns.set_style("whitegrid")
+    sns.set(font_scale = 0.7)
+    ax = sns.barplot(x="routing_key", y="counts", data=freqdf)
+    plt.show()
+    print('\n' + "MESSAGES DICT: " + "\n")
+    print(message_dict)
+
+
+connection.close()
+
--- AMQP/send.py
+++ AMQP/send.py
@@ -5,10 +5,22 @@ connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost')
 channel = connection.channel()
 
 
-channel.queue_declare(queue='hello')
+channel.exchange_declare(exchange='topics',
+                         exchange_type='topic')
 
-channel.basic_publish(exchange='',
-                      routing_key='hello',
-                      body='Hello World!')
-print(" [x] Sent 'Hello World!'")
+exit = True
+while exit:
+	try:
+		message = input("SEND A MESSAGE: " + '\n')
+		routing_key = input("enter a routing key: " + '\n')
+		if len(routing_key) < 3:
+			routing_key = "default"
+
+		channel.basic_publish(exchange='topics',
+	                      routing_key=routing_key,
+	                      body=message)
+		print(" [x] Sent %r:%r" % (routing_key, message))
+	except KeyboardInterrupt:
+		print("EXITED")
+		break
 connection.close()
